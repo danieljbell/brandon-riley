098ce3c345ae2cbdc1f3bb2efd6e03fb78af6fb0
danieljbell <Daniel@MacBook-Pro.local>
2015-07-13 20:16:37 -0500

    padding to container

--- js/site.js
+++ js/site.js
@@ -20,7 +20,39 @@ $(document).ready( function() {
 		}
 	})
 
-	$(".main-vid").fitVids();
+	// Start of Video Grid
+    $('div.grid').click(function (event) {
+        var current = $('#video-grid-14280 .current-video').attr('data-current');
+        var $this = $($(event.target).parents('.video')[0]);
+        var key = $this.attr('data-key');
+        if(key == current) {
+            return;
+        }
+        var videoId = $this.attr('data-id');
+        $('#video-grid-14280 div.current-video iframe').attr('src', '//www.youtube-nocookie.com/embed/' + videoId + '?rel=0&showinfo=0&modestbranding=1&autohide=1&vq=hd720');
+        $('#video-grid-14280 div.current-video .long-description').remove();
+        var description = $('.long-description', $this).clone();
+        $('#video-grid-14280 .current-video').append(description);
+        var current = $('#video-grid-14280 .current-video').attr('data-current', key);
+        description.show();
+    });
+
+
+    $('.video-grid img').load(function() {
+        $(document).ready(function() {
+            var height = 0;
+            $('#video-grid-14280 div.video').each(function () {
+                var $thisHeight = $(this).height();
+                if ($thisHeight > height) {
+                    height = $thisHeight;
+                }
+            });
+            $('#video-grid-14280 div.video').height(height);
+        });
+    });
+
+
+
 })
 
 function parallax() {
