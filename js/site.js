642a54e06215810be6cea1810e4d7b1c845706c5
danieljbell <Daniel@MacBook-Pro.local>
2015-07-17 16:54:44 -0500

    updated a bunch of things
    Finished Video Grid, added in contact form

--- js/site.js
+++ js/site.js
@@ -18,38 +18,10 @@ $(document).ready( function() {
 		}
 	})
 
-	// Start of Video Grid
-    // $('div.grid').click(function (event) {
-    //     // var current = $('#video-grid-14280 .current-video').attr('data-current');
-    //     // var $this = $($(event.target).parents('.video')[0]);
-    //     // var key = $this.attr('data-key');
-    //     // if(key == current) {
-    //     //     return;
-    //     // }
-    //     var videoId = $this.attr('data-id');
-    //     $('#video-grid-14280 div.current-video iframe').attr('src', '//www.youtube-nocookie.com/embed/' + videoId + '?rel=0&showinfo=0&modestbranding=1&autohide=1&vq=hd720');
-        
-    // });
-
-
-    // $('.video-grid img').load(function() {
-    //     $(document).ready(function() {
-    //         var height = 0;
-    //         $('#video-grid-14280 div.video').each(function () {
-    //             var $thisHeight = $(this).height();
-    //             if ($thisHeight > height) {
-    //                 height = $thisHeight;
-    //             }
-    //         });
-    //         $('#video-grid-14280 div.video').height(height);
-    //     });
-    // });
-
-
 $(".various").fancybox({
-        maxWidth    : 1280,
+        maxWidth    : 1265,
         maxHeight   : 720,
-        fitToView   : false,
+        fitToView   : true,
         width       : '100%',
         height      : '100%',
         autoSize    : false,
