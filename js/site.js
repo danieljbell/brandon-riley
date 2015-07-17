39082553bef363d157c6017e8a1e4e463f05b54b
danieljbell <Daniel@MacBook-Pro.local>
2015-07-17 14:45:08 -0500

    added fancybox.js

--- js/site.js
+++ js/site.js
@@ -19,36 +19,44 @@ $(document).ready( function() {
 	})
 
 	// Start of Video Grid
-    $('div.grid').click(function (event) {
-        var current = $('#video-grid-14280 .current-video').attr('data-current');
-        var $this = $($(event.target).parents('.video')[0]);
-        var key = $this.attr('data-key');
-        if(key == current) {
-            return;
-        }
-        var videoId = $this.attr('data-id');
-        $('#video-grid-14280 div.current-video iframe').attr('src', '//www.youtube-nocookie.com/embed/' + videoId + '?rel=0&showinfo=0&modestbranding=1&autohide=1&vq=hd720');
-        $('#video-grid-14280 div.current-video .long-description').remove();
-        var description = $('.long-description', $this).clone();
-        $('#video-grid-14280 .current-video').append(description);
-        var current = $('#video-grid-14280 .current-video').attr('data-current', key);
-        description.show();
+    // $('div.grid').click(function (event) {
+    //     // var current = $('#video-grid-14280 .current-video').attr('data-current');
+    //     // var $this = $($(event.target).parents('.video')[0]);
+    //     // var key = $this.attr('data-key');
+    //     // if(key == current) {
+    //     //     return;
+    //     // }
+    //     var videoId = $this.attr('data-id');
+    //     $('#video-grid-14280 div.current-video iframe').attr('src', '//www.youtube-nocookie.com/embed/' + videoId + '?rel=0&showinfo=0&modestbranding=1&autohide=1&vq=hd720');
+        
+    // });
+
+
+    // $('.video-grid img').load(function() {
+    //     $(document).ready(function() {
+    //         var height = 0;
+    //         $('#video-grid-14280 div.video').each(function () {
+    //             var $thisHeight = $(this).height();
+    //             if ($thisHeight > height) {
+    //                 height = $thisHeight;
+    //             }
+    //         });
+    //         $('#video-grid-14280 div.video').height(height);
+    //     });
+    // });
+
+
+$(".various").fancybox({
+        maxWidth    : 1280,
+        maxHeight   : 720,
+        fitToView   : false,
+        width       : '100%',
+        height      : '100%',
+        autoSize    : false,
+        closeClick  : false,
+        openEffect  : 'fade',
+        closeEffect : 'fade'
     });
-
-
-    $('.video-grid img').load(function() {
-        $(document).ready(function() {
-            var height = 0;
-            $('#video-grid-14280 div.video').each(function () {
-                var $thisHeight = $(this).height();
-                if ($thisHeight > height) {
-                    height = $thisHeight;
-                }
-            });
-            $('#video-grid-14280 div.video').height(height);
-        });
-    });
-
 
 
 })
